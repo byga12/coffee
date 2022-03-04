25cf6c8d39d52b07be52545c98bf0e319701c695
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-03-04 12:31:26 -0300

    changes

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -4,6 +4,10 @@ import l from "./Contact.module.css"
 import { FaFacebook,FaInstagram,FaTwitterSquare } from "react-icons/fa";
 import photo from '../../../assets/granos-de-cafe-1.jpg'
 import Title from '../../Title/Title';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
 
 
 const Contact = () => {
@@ -16,22 +20,76 @@ const Contact = () => {
         También podés llamarnos sin cargo al 0800-999-3000 o escribirnos un mail a sugerencias@devffee.com.ar
         </section>
         <div className={l.container}>
+        
            <p>Motivo de contacto</p> 
-           <input></input>
+             <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField fullWidth label="Motivo de Contacto" id="fullWidth"  variant="filled"  />
+            </Box>
            <p>Mensaje</p> 
-           <input></input>
+           <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField fullWidth label="Mensaje" id="fullWidth" variant="filled" />
+            </Box>
            <p>Nombre y apellido</p> 
-           <input></input>
+           <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField fullWidth label="Nombre y apellido" id="fullWidth" variant="filled" />
+            </Box>
            <p>Telefono</p> 
-           <input></input>
+           <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField fullWidth label="Telefono" id="fullWidth" variant="filled"  />
+            </Box>
            <p>Email</p> 
-           <input></input>
+           <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField fullWidth label="Email" id="fullWidth"  variant="filled"/>
+            </Box>
+
+            <Button variant="contained" sx={{color:'white', background:'black', position: 'relative', left: '80%'}} >Enviar</Button>
         </div>
+
         <h1>Redes</h1> 
-        <FaFacebook>
-        </FaFacebook>
-        <FaInstagram></FaInstagram>
-        <FaTwitterSquare></FaTwitterSquare>
+        <Box sx={{ width: 1 }}>
+          <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+            <Box gridColumn="span 4">
+           
+                <FaFacebook className={l.icons} />
+               
+            </Box>
+            <Box gridColumn="span 4">
+                <FaInstagram className={l.icons} />
+            </Box>
+            <Box gridColumn="span 4">
+                  <FaTwitterSquare className={l.icons}/>
+            </Box>
+            
+          </Box>
+    </Box>
+
+         
+             
       </div>
     </div>
 
--- src/Components/Pages/Franchises/Franchises.js
+++ src/Components/Pages/Franchises/Franchises.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Title from '../../Title/Title'
 import photo from '../../../assets/granos-de-cafe-1.jpg'
+import s from './Franchises.module.css'
 
 const Franchises = () => {
   return (
     <div>
       <Title imageUrl={photo} titleText='Franquicias'/>
+      <div className={s.container}>
+          <div>
+              
+          </div>
+          <div>
+              
+          </div>
+          
+      </div>
     </div>
   )
 }
