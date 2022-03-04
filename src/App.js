519ea906d3653b545bb31d0a96b2ba1391b3ba9e
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-03-04 17:22:37 -0300

    LazyLoader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Suspense , lazy} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./Components/Pages/About/About";
-import Home from "./Components/Pages/Home/Home";
-import Products from "./Components/Pages/Products/Products";
-import Layout from "./Components/Pages/Layout";
-import Sucursales from "./Components/Pages/Sucursales/Sucursales";
-import Contact from "./Components/Pages/Contact/Contact";
-import Franchises from "./Components/Pages/Franchises/Franchises";
+import LazyLoad from "./Components/LazyLoad/LazyLoad";
+const About = lazy(() => import("./Components/Pages/About/About")); 
+const Home = lazy(() => import("./Components/Pages/Home/Home")); 
+const Products = lazy(() => import("./Components/Pages/Products/Products")); 
+const Layout = lazy(() => import("./Components/Pages/Layout")); 
+const Sucursales = lazy(() => import("./Components/Pages/Sucursales/Sucursales")); 
+const Contact = lazy(() => import("./Components/Pages/Contact/Contact")); 
+const Franchises  = lazy(() => import("./Components/Pages/Franchises/Franchises")); 
+
+
 
 const App = () => {
   return (
     <>
+    <Suspense fallback={<LazyLoad></LazyLoad>}>
       <BrowserRouter>
       <Layout>
         <Routes>
@@ -23,6 +27,7 @@ const App = () => {
         </Routes>
         </Layout>
       </BrowserRouter>
+      </Suspense>
     </>
   );
 
